2c31e9391d9dc2685d003e1e0bc3e620f982e649
Pranil Rathod <rathod1337@gmail.com>
2021-03-01 06:38:21 +0530

    #Feat - added auth response model

---
+++ src/main/java/covid19/backend/Models/AuthResponse.java
@@ -0,0 +1,30 @@
+package covid19.backend.Models;
+
+public class AuthResponse {
+    private String email;
+    private String token;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String email, String token) {
+        this.email = email;
+        this.token = token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
