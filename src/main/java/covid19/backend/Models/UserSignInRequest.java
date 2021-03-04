4181d584134c1890720d2e9efba17891891aa213
Pranil Rathod <rathod1337@gmail.com>
2021-03-04 15:08:18 +0530

    #Feat - added signin request data model

---
+++ src/main/java/covid19/backend/Models/UserSignInRequest.java
@@ -0,0 +1,31 @@
+package covid19.backend.Models;
+
+public class UserSignInRequest {
+    private String username;
+    private String password;
+
+    public UserSignInRequest() {
+    }
+
+    public UserSignInRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
