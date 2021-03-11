d801aec0965b4baf0242ab3a4dd812c44a7a627b
Pranil Rathod <rathod1337@gmail.com>
2021-03-11 13:44:54 +0530

    #Refactor - added resttemplate bean

--- src/main/java/covid19/backend/BackendApplication.java
+++ src/main/java/covid19/backend/BackendApplication.java
@@ -2,6 +2,10 @@ package covid19.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import java.time.Duration;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +14,13 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+		return builder
+				.defaultHeader("Content-Type","application/json")
+				.setConnectTimeout(Duration.ofMillis(3000))
+				.setReadTimeout(Duration.ofMillis(3000))
+				.build();
+	}
 }
