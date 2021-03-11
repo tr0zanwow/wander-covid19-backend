bbc89ddb88021ba490bc1e61647b64775c37d3e2
Pranil Rathod <rathod1337@gmail.com>
2021-03-11 14:18:27 +0530

    #Refactor - renamed some properties

--- src/main/java/covid19/backend/Models/UserSignInRequest.java
+++ src/main/java/covid19/backend/Models/UserSignInRequest.java
@@ -1,23 +1,23 @@
 package covid19.backend.Models;
 
 public class UserSignInRequest {
-    private String username;
+    private String email;
     private String password;
 
     public UserSignInRequest() {
     }
 
-    public UserSignInRequest(String username, String password) {
-        this.username = username;
+    public UserSignInRequest(String email, String password) {
+        this.email = email;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public String getemail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setemail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
