26456244233738d4e4f2a384932679c88a076876
Pranil Rathod <rathod1337@gmail.com>
2021-03-11 13:49:36 +0530

    #Refactor - added methods to sync latest data, sync coordinates & sync all data

--- src/main/java/covid19/backend/Configuration/CovidDataSyncConfiguration.java
+++ src/main/java/covid19/backend/Configuration/CovidDataSyncConfiguration.java
@@ -1,14 +1,33 @@
 package covid19.backend.Configuration;
 
+import covid19.backend.Services.CovidDataSyncService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
 
 @Configuration
 @EnableScheduling
 public class CovidDataSyncConfiguration {
 
-    @Scheduled(cron = "0 57 21 * * *", zone = "GMT+5:30")
-    public void syncCovidData() {
+    @Autowired
+    CovidDataSyncService dataSyncService;
+
+    @Scheduled(cron = "0 0 21 * * *", zone = "GMT+5:30")
+    public void syncLatestData() throws Exception {
+//
+    }
+
+    public void syncCoordinates() throws IOException {
+        dataSyncService.getStateCoordinates();
+        dataSyncService.getDistrictCoordinates();
+    }
+
+    public void syncAllData() throws Exception {
+        dataSyncService.getDataStateWise();
+        dataSyncService.getDataNation();
+        dataSyncService.getDataDistrictWise();
     }
 }
