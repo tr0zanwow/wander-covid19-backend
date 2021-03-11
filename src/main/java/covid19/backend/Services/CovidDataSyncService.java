a8ff500e519307dc1d79720fc618ad9e78035a9c
Pranil Rathod <rathod1337@gmail.com>
2021-03-12 01:41:27 +0530

    #Refactor - added video news data method

--- src/main/java/covid19/backend/Services/CovidDataSyncService.java
+++ src/main/java/covid19/backend/Services/CovidDataSyncService.java
@@ -1,11 +1,18 @@
 package covid19.backend.Services;
 
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import covid19.backend.Models.Coordinates;
 import covid19.backend.Models.Covid19.*;
 import covid19.backend.Models.GeoCodeCoordinates;
 import covid19.backend.Models.MongoDB.*;
+import covid19.backend.Models.NewsAPI;
 import covid19.backend.Repository.Covid19.*;
 import covid19.backend.Repository.GeoCoding;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +23,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -45,6 +55,9 @@ public class CovidDataSyncService {
     NewsAPIMongoRepository newsAPIMongoRepository;
 
     @Autowired
+    VideoNewsMongoRepository videoNewsMongoRepository;
+
+    @Autowired
     GeoCoding geoCoding;
 
     @Autowired
@@ -54,6 +67,7 @@ public class CovidDataSyncService {
     private static final String STATE_SERIES_URL = "https://api.covid19india.org/csv/latest/state_wise_daily.csv";
     private static final String NATION_SERIES_URL = "https://api.covid19india.org/csv/latest/case_time_series.csv";
     private static final String DISTRICT_WISE_URL = "https://api.covid19india.org/csv/latest/district_wise.csv";
+    private static final long MAX_SEARCH_RESULTS = 20;
 
     private BufferedReader getURLReader(String url) throws IOException {
         URL dataUrl = new URL(url);
@@ -169,7 +183,7 @@ public class CovidDataSyncService {
         newsAPIMongoRepository.deleteAll();
         List<MongoCasesStateWise> casesStateWises = stateWiseMongoRepository.findAll();
         for (MongoCasesStateWise csw : casesStateWises.subList(1, casesStateWises.size() - 1)) {
-            NewsAPI newsAPI = restTemplate.getForObject("http://newsapi.org/v2/everything?q=covid19 "+csw.getState()+"&from=2021-03-01&sortBy=publishedAt&apiKey=898b8147285c4367a43df902a0196149", NewsAPI.class);
+            NewsAPI newsAPI = restTemplate.getForObject("http://newsapi.org/v2/everything?q=covid19 " + csw.getState() + "&from=2021-03-01&sortBy=publishedAt&apiKey=898b8147285c4367a43df902a0196149", NewsAPI.class);
 
             List<NewsAPI.Article> articles = newsAPI.getArticles();
             List<MongoCovidNews.Articles> mongoCovidArticles = new ArrayList<>();
@@ -186,6 +200,76 @@ public class CovidDataSyncService {
             MongoCovidNews covidNews = new MongoCovidNews(csw.getState(), mongoCovidArticles);
 
             newsAPIMongoRepository.save(covidNews);
+        }
+    }
+
+    private String buildVideoUrl(String videoId) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("https://www.youtube.com/watch?v=");
+        builder.append(videoId);
+
+        return builder.toString();
+    }
+
+    private YouTube getYouTube() {
+        YouTube youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(),
+                (request) -> {
+                }).setApplicationName("youtube-spring-boot-demo").build();
+
+        return youtube;
+    }
+
+    public void getVideoNewsData() {
+        videoNewsMongoRepository.deleteAll();
+        List<MongoCasesStateWise> casesStateWises = stateWiseMongoRepository.findAll();
+        for (MongoCasesStateWise csw : casesStateWises.subList(1, casesStateWises.size() - 1)) {
+
+            String queryTerm = "Covid 19 " + csw.getState() + " news";
+            List<MongoCovidVideoNews.VideoNews> videos = new ArrayList<>();
+
+            try {
+                YouTube youtube = getYouTube();
+
+                YouTube.Search.List search = youtube.search().list("id,snippet");
+
+                String apiKey = "AIzaSyBLvPpgBnh8TeRg-JACCuriO6w1fcPEFmw";
+                search.setKey(apiKey);
+
+                search.setQ(queryTerm);
+
+                search.setType("video");
+
+                search.setFields("items(id/kind,id/videoId,snippet/title,snippet/description,snippet/publishedAt,snippet/thumbnails/default/url,snippet/channelTitle)");
+
+                search.setMaxResults(MAX_SEARCH_RESULTS);
+
+                DateFormat df = new SimpleDateFormat("MMM dd, yyyy");
+
+                SearchListResponse searchResponse = search.execute();
+                List<SearchResult> searchResultList = searchResponse.getItems();
+                if (searchResultList != null) {
+                    for (SearchResult result : searchResultList) {
+                        MongoCovidVideoNews.VideoNews video = new MongoCovidVideoNews.VideoNews();
+                        video.setTitle(result.getSnippet().getTitle());
+                        video.setUrl(buildVideoUrl(result.getId().getVideoId()));
+                        video.setThumbnailUrl(result.getSnippet().getThumbnails().getDefault().getUrl());
+                        video.setDescription(result.getSnippet().getDescription());
+                        video.setChannelName(result.getSnippet().getChannelTitle());
+                        DateTime dateTime = result.getSnippet().getPublishedAt();
+                        Date date = new Date(dateTime.getValue());
+                        String dateString = df.format(date);
+                        video.setPublishDate(dateString);
+
+                        videos.add(video);
+                    }
+                    MongoCovidVideoNews mongoCovidVideoNews = new MongoCovidVideoNews(csw.getState(), videos);
+                    videoNewsMongoRepository.save(mongoCovidVideoNews);
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
