34f4be8db705819c8da7d7e4a24ac74dd5f5e12e
Pranil Rathod <rathod1337@gmail.com>
2021-03-11 14:17:39 +0530

    #Refactor - added method to register, login and verify user

--- src/main/java/covid19/backend/Services/UserAuthService.java
+++ src/main/java/covid19/backend/Services/UserAuthService.java
@@ -1,29 +1,91 @@
 package covid19.backend.Services;
 
+import covid19.backend.Models.UserSignInRequest;
+import covid19.backend.Models.UserSignUpResponse;
+import covid19.backend.Repository.SMTPEmailRepository;
+import covid19.backend.Repository.UserSignUpMongoRepository;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import covid19.backend.Models.UserSignUpRequest;
-import covid19.backend.Repository.UserAuthRepository;
-
-import java.util.ArrayList;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.mail.MessagingException;
+import java.util.*;
 
 @Service
 public class UserAuthService implements UserDetailsService {
+    @Autowired
+    UserSignUpMongoRepository userSignUpMongoRepository;
 
     @Autowired
-    UserAuthRepository userAuthRepository;
+    SMTPEmailRepository emailRepository;
+
+    ModelAndView mv;
 
     @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        UserSignUpRequest retrievedUserSignUpRequest = userAuthRepository.findByUserName(userName);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserSignUpRequest retrievedUserSignUpRequest = userSignUpMongoRepository.findByEmail(email);
         if (retrievedUserSignUpRequest == null) {
-            throw new UsernameNotFoundException("No found user with email: " + userName);
+            throw new UsernameNotFoundException("No found user with email: " + email);
         }
-        return new org.springframework.security.core.userdetails.User(retrievedUserSignUpRequest.getEmail(), retrievedUserSignUpRequest.getPassword(),new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(retrievedUserSignUpRequest.getEmail(), retrievedUserSignUpRequest.getPassword(), new ArrayList<>());
+    }
+
+    public UserSignUpResponse registerUser(UserSignUpRequest userSignUpRequest) throws MessagingException {
+        if (!userSignUpMongoRepository.findByEmail(userSignUpRequest.getEmail()).getEmail().equals(userSignUpRequest.getEmail())) {
+            userSignUpRequest.setHashedPassword();
+            String token = UUID.randomUUID().toString();
+            userSignUpRequest.setverificationToken(token);
+            userSignUpMongoRepository.save(userSignUpRequest);
+            final String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+            String tokenUrl = baseUrl + "/verifyaccount?token=" + token;
+            emailRepository.sendMail(userSignUpRequest.getEmail(), tokenUrl);
+
+            return new UserSignUpResponse(userSignUpRequest.getUuid(), userSignUpRequest.getEmail());
+        } else {
+            throw new ResponseStatusException(HttpStatus.SC_CONFLICT,
+                    "Account already exists with email: " + userSignUpRequest.getEmail(), null);
+        }
+    }
+
+    public ModelAndView verifyUserToken(String token) {
+        UserSignUpRequest foundUser = userSignUpMongoRepository.findByVerificationToken(token);
+        if (foundUser.getverificationToken().equals(token)) {
+            if (!foundUser.getEnabled()) {
+                foundUser.setEnabled(true);
+                userSignUpMongoRepository.save(foundUser);
+                mv = new ModelAndView("token_verified");
+                mv.addObject("email", foundUser.getEmail());
+                return mv;
+            } else {
+                mv = new ModelAndView("token_error_already_verified");
+                return mv;
+            }
+        } else {
+            mv = new ModelAndView("token_error");
+            return mv;
+        }
+    }
+
+    public Boolean login(UserSignInRequest userSignInRequest) {
+
+        UserSignUpRequest foundUser = userSignUpMongoRepository.findByEmail(userSignInRequest.getemail());
+        if (foundUser != null && foundUser.getEmail().equals(userSignInRequest.getemail())) {
+            if (Boolean.FALSE.equals(foundUser.verifyHashedPassword(userSignInRequest.getPassword()))) {
+                throw new ResponseStatusException(HttpStatus.SC_UNAUTHORIZED, "Wrong Credentials", null);
+            } else if (Boolean.FALSE.equals(foundUser.getEnabled())) {
+                throw new ResponseStatusException(HttpStatus.SC_FORBIDDEN, "Account not activated", null);
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.SC_NOT_FOUND,
+                    "No account found with email: " + userSignInRequest.getemail(), null);
+        }
+        return true;
     }
 
 }
