9124572ab1433a2ed8b6ae9502e7f9bc20b50726
Pranil Rathod <rathod1337@gmail.com>
2021-03-14 05:40:34 +0530

    #Refactor - added cors config to allow cross origin support

--- src/main/java/covid19/backend/Configuration/SecurityConfigurer.java
+++ src/main/java/covid19/backend/Configuration/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package covid19.backend.Configuration;
 
+import covid19.backend.Filter.ExceptionHandlerFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,11 +14,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import covid19.backend.Filter.JWTRequestFilter;
 import covid19.backend.Misc.MongoDBAuthProvider;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     JWTRequestFilter jwtRequestFilter;
+
+    @Autowired
+    ExceptionHandlerFilter exceptionHandlerFilter;
 
     @Autowired
     MongoDBAuthProvider authProvider;
@@ -35,13 +45,27 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/signup", "/signin", "/verifyaccount","/getdata","/synccoordinates","/syncdata").permitAll().anyRequest().authenticated()
-                .and().formLogin().permitAll()
+        http.authorizeRequests().antMatchers("/signup", "/signin", "/verifyaccount").permitAll().anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        http.csrf().disable();
+        http.addFilterBefore(exceptionHandlerFilter, JWTRequestFilter.class);
+        http.csrf().disable().cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("http://localhost:8081");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
