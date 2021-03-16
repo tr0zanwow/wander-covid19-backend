489bdacf13b2f6663b2dff46c9bb068e0b804ff2
Pranil Rathod <rathod1337@gmail.com>
2021-03-17 01:08:14 +0530

    #Refactor - added findbystate method

--- src/main/java/covid19/backend/Repository/Covid19/StateCoordinatesMongoRepository.java
+++ src/main/java/covid19/backend/Repository/Covid19/StateCoordinatesMongoRepository.java
@@ -4,4 +4,5 @@ import covid19.backend.Models.MongoDB.MongoStateCoordinates;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface StateCoordinatesMongoRepository extends MongoRepository<MongoStateCoordinates,String> {
+    MongoStateCoordinates findByState(String state);
 }
