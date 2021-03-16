511c8e4d8eadf4af9e5880dc16d29231a58034d9
Pranil Rathod <rathod1337@gmail.com>
2021-03-17 01:07:55 +0530

    #Refactor - Removed unnecessary imports

--- src/main/java/covid19/backend/Configuration/SecurityConfigurer.java
+++ src/main/java/covid19/backend/Configuration/SecurityConfigurer.java
@@ -17,7 +17,6 @@ import covid19.backend.Misc.MongoDBAuthProvider;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
