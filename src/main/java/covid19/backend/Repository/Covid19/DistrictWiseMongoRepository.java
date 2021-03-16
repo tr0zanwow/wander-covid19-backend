d7ade189c5df9d214f6ec1a9d4d1c57ecff3a78b
Pranil Rathod <rathod1337@gmail.com>
2021-03-17 01:06:23 +0530

    #Refactor - added find by state method

--- src/main/java/covid19/backend/Repository/Covid19/DistrictWiseMongoRepository.java
+++ src/main/java/covid19/backend/Repository/Covid19/DistrictWiseMongoRepository.java
@@ -1,8 +1,9 @@
 package covid19.backend.Repository.Covid19;
 
 import covid19.backend.Models.MongoDB.MongoCasesDistrictWise;
-import covid19.backend.Models.MongoDB.MongoCasesStateWise;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface DistrictWiseMongoRepository extends MongoRepository<MongoCasesDistrictWise,String> {
+    List<MongoCasesDistrictWise> findByState(String state);
 }
