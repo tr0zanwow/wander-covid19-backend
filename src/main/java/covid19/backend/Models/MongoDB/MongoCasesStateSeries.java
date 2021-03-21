d0a550230d02b416d0b27d2514c40a7c306049df
Pranil Rathod <rathod1337@gmail.com>
2021-03-21 23:38:27 +0530

    #Refactor - restructured case state series mongo data model

--- src/main/java/covid19/backend/Models/MongoDB/MongoCasesStateSeries.java
+++ src/main/java/covid19/backend/Models/MongoDB/MongoCasesStateSeries.java
@@ -3,98 +3,24 @@ package covid19.backend.Models.MongoDB;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Document(collection = "data_state_series")
 public class MongoCasesStateSeries {
 
     @MongoId
     private String _id;
-    private String date;
-    private String status;
-    private int tT;
-    private int aN;
-    private int aP;
-    private int aR;
-    private int aS;
-    private int bR;
-    private int cH;
-    private int cT;
-    private int dN;
-    private int dD;
-    private int dL;
-    private int gA;
-    private int gJ;
-    private int hR;
-    private int hP;
-    private int jK;
-    private int jH;
-    private int kA;
-    private int kL;
-    private int lA;
-    private int lD;
-    private int mP;
-    private int mH;
-    private int mN;
-    private int mL;
-    private int mZ;
-    private int nL;
-    private int oR;
-    private int pY;
-    private int pB;
-    private int rJ;
-    private int sK;
-    private int tN;
-    private int tG;
-    private int tR;
-    private int uP;
-    private int uT;
-    private int wB;
-    private int uN;
+    private String state;
+    private List<DailySeries> series;
 
     public MongoCasesStateSeries() {
+        this.series = new ArrayList<>();
     }
 
-    public MongoCasesStateSeries(String date, String status, int tT, int aN, int aP, int aR, int aS, int bR, int cH, int cT, int dN, int dD, int dL, int gA, int gJ, int hR, int hP, int jK, int jH, int kA, int kL, int lA, int lD, int mP, int mH, int mN, int mL, int mZ, int nL, int oR, int pY, int pB, int rJ, int sK, int tN, int tG, int tR, int uP, int uT, int wB, int uN) {
-        this.date = date;
-        this.status = status;
-        this.tT = tT;
-        this.aN = aN;
-        this.aP = aP;
-        this.aR = aR;
-        this.aS = aS;
-        this.bR = bR;
-        this.cH = cH;
-        this.cT = cT;
-        this.dN = dN;
-        this.dD = dD;
-        this.dL = dL;
-        this.gA = gA;
-        this.gJ = gJ;
-        this.hR = hR;
-        this.hP = hP;
-        this.jK = jK;
-        this.jH = jH;
-        this.kA = kA;
-        this.kL = kL;
-        this.lA = lA;
-        this.lD = lD;
-        this.mP = mP;
-        this.mH = mH;
-        this.mN = mN;
-        this.mL = mL;
-        this.mZ = mZ;
-        this.nL = nL;
-        this.oR = oR;
-        this.pY = pY;
-        this.pB = pB;
-        this.rJ = rJ;
-        this.sK = sK;
-        this.tN = tN;
-        this.tG = tG;
-        this.tR = tR;
-        this.uP = uP;
-        this.uT = uT;
-        this.wB = wB;
-        this.uN = uN;
+    public MongoCasesStateSeries(String state, List<DailySeries> series) {
+        this.state = state;
+        this.series = series;
     }
 
     public String get_id() {
@@ -105,331 +31,68 @@ public class MongoCasesStateSeries {
         this._id = _id;
     }
 
-    public String getDate() {
-        return date;
+    public String getState() {
+        return state;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setState(String state) {
+        this.state = state;
     }
 
-    public String getStatus() {
-        return status;
+    public List<DailySeries> getSeries() {
+        return series;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setSeries(List<DailySeries> series) {
+        this.series = series;
     }
 
-    public int gettT() {
-        return tT;
-    }
-
-    public void settT(int tT) {
-        this.tT = tT;
-    }
-
-    public int getaN() {
-        return aN;
-    }
-
-    public void setaN(int aN) {
-        this.aN = aN;
-    }
-
-    public int getaP() {
-        return aP;
-    }
-
-    public void setaP(int aP) {
-        this.aP = aP;
-    }
-
-    public int getaR() {
-        return aR;
-    }
-
-    public void setaR(int aR) {
-        this.aR = aR;
-    }
-
-    public int getaS() {
-        return aS;
-    }
-
-    public void setaS(int aS) {
-        this.aS = aS;
-    }
-
-    public int getbR() {
-        return bR;
-    }
-
-    public void setbR(int bR) {
-        this.bR = bR;
-    }
-
-    public int getcH() {
-        return cH;
-    }
-
-    public void setcH(int cH) {
-        this.cH = cH;
-    }
-
-    public int getcT() {
-        return cT;
-    }
-
-    public void setcT(int cT) {
-        this.cT = cT;
-    }
-
-    public int getdN() {
-        return dN;
-    }
-
-    public void setdN(int dN) {
-        this.dN = dN;
-    }
-
-    public int getdD() {
-        return dD;
-    }
-
-    public void setdD(int dD) {
-        this.dD = dD;
-    }
-
-    public int getdL() {
-        return dL;
-    }
-
-    public void setdL(int dL) {
-        this.dL = dL;
-    }
-
-    public int getgA() {
-        return gA;
-    }
-
-    public void setgA(int gA) {
-        this.gA = gA;
-    }
-
-    public int getgJ() {
-        return gJ;
-    }
-
-    public void setgJ(int gJ) {
-        this.gJ = gJ;
-    }
-
-    public int gethR() {
-        return hR;
-    }
-
-    public void sethR(int hR) {
-        this.hR = hR;
-    }
-
-    public int gethP() {
-        return hP;
-    }
-
-    public void sethP(int hP) {
-        this.hP = hP;
-    }
-
-    public int getjK() {
-        return jK;
-    }
-
-    public void setjK(int jK) {
-        this.jK = jK;
-    }
-
-    public int getjH() {
-        return jH;
-    }
-
-    public void setjH(int jH) {
-        this.jH = jH;
-    }
-
-    public int getkA() {
-        return kA;
-    }
-
-    public void setkA(int kA) {
-        this.kA = kA;
-    }
-
-    public int getkL() {
-        return kL;
-    }
-
-    public void setkL(int kL) {
-        this.kL = kL;
-    }
-
-    public int getlA() {
-        return lA;
-    }
-
-    public void setlA(int lA) {
-        this.lA = lA;
-    }
-
-    public int getlD() {
-        return lD;
-    }
-
-    public void setlD(int lD) {
-        this.lD = lD;
-    }
-
-    public int getmP() {
-        return mP;
-    }
-
-    public void setmP(int mP) {
-        this.mP = mP;
-    }
-
-    public int getmH() {
-        return mH;
-    }
-
-    public void setmH(int mH) {
-        this.mH = mH;
-    }
-
-    public int getmN() {
-        return mN;
-    }
-
-    public void setmN(int mN) {
-        this.mN = mN;
-    }
-
-    public int getmL() {
-        return mL;
-    }
-
-    public void setmL(int mL) {
-        this.mL = mL;
-    }
-
-    public int getmZ() {
-        return mZ;
-    }
-
-    public void setmZ(int mZ) {
-        this.mZ = mZ;
-    }
-
-    public int getnL() {
-        return nL;
-    }
-
-    public void setnL(int nL) {
-        this.nL = nL;
-    }
-
-    public int getoR() {
-        return oR;
-    }
-
-    public void setoR(int oR) {
-        this.oR = oR;
-    }
-
-    public int getpY() {
-        return pY;
-    }
-
-    public void setpY(int pY) {
-        this.pY = pY;
-    }
-
-    public int getpB() {
-        return pB;
-    }
-
-    public void setpB(int pB) {
-        this.pB = pB;
-    }
-
-    public int getrJ() {
-        return rJ;
-    }
-
-    public void setrJ(int rJ) {
-        this.rJ = rJ;
-    }
-
-    public int getsK() {
-        return sK;
-    }
-
-    public void setsK(int sK) {
-        this.sK = sK;
-    }
-
-    public int gettN() {
-        return tN;
-    }
-
-    public void settN(int tN) {
-        this.tN = tN;
-    }
-
-    public int gettG() {
-        return tG;
-    }
-
-    public void settG(int tG) {
-        this.tG = tG;
-    }
+    public static class DailySeries {
+        private String date;
+        private String confirmed;
+        private String recovered;
+        private String deceased;
 
-    public int gettR() {
-        return tR;
-    }
+        public DailySeries() {
+        }
 
-    public void settR(int tR) {
-        this.tR = tR;
-    }
+        public DailySeries(String date, String confirmed, String recovered, String deceased) {
+            this.date = date;
+            this.confirmed = confirmed;
+            this.recovered = recovered;
+            this.deceased = deceased;
+        }
 
-    public int getuP() {
-        return uP;
-    }
+        public String getDate() {
+            return date;
+        }
 
-    public void setuP(int uP) {
-        this.uP = uP;
-    }
+        public void setDate(String date) {
+            this.date = date;
+        }
 
-    public int getuT() {
-        return uT;
-    }
+        public String getConfirmed() {
+            return confirmed;
+        }
 
-    public void setuT(int uT) {
-        this.uT = uT;
-    }
+        public void setConfirmed(String confirmed) {
+            this.confirmed = confirmed;
+        }
 
-    public int getwB() {
-        return wB;
-    }
+        public String getRecovered() {
+            return recovered;
+        }
 
-    public void setwB(int wB) {
-        this.wB = wB;
-    }
+        public void setRecovered(String recovered) {
+            this.recovered = recovered;
+        }
 
-    public int getuN() {
-        return uN;
-    }
+        public String getDeceased() {
+            return deceased;
+        }
 
-    public void setuN(int uN) {
-        this.uN = uN;
+        public void setDeceased(String deceased) {
+            this.deceased = deceased;
+        }
     }
 }
