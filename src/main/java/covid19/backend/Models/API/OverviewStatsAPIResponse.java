8e0625619de8ec46a52d9761f9c3a894c65477a3
Pranil Rathod <rathod1337@gmail.com>
2021-03-21 23:44:18 +0530

    #Refactor - restructured overview stats response model

--- src/main/java/covid19/backend/Models/API/OverviewStatsAPIResponse.java
+++ src/main/java/covid19/backend/Models/API/OverviewStatsAPIResponse.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class OverviewStatsAPIResponse {
     private OverviewStats statistics;
-    private List<OverviewStats> series;
+    private List<OverviewSeries> series;
     private List<OverviewNews> news;
 
     public OverviewStatsAPIResponse() {
     }
 
-    public OverviewStatsAPIResponse(OverviewStats statistics, List<OverviewStats> series, List<OverviewNews> news) {
+    public OverviewStatsAPIResponse(OverviewStats statistics, List<OverviewSeries> series, List<OverviewNews> news) {
         this.statistics = statistics;
         this.series = series;
         this.news = news;
@@ -24,11 +24,11 @@ public class OverviewStatsAPIResponse {
         this.statistics = statistics;
     }
 
-    public List<OverviewStats> getSeries() {
+    public List<OverviewSeries> getSeries() {
         return series;
     }
 
-    public void setSeries(List<OverviewStats> series) {
+    public void setSeries(List<OverviewSeries> series) {
         this.series = series;
     }
 
@@ -42,18 +42,26 @@ public class OverviewStatsAPIResponse {
 
     public static class OverviewStats {
         private String date;
+        private String active;
         private String confirmed;
         private String recovered;
         private String deceased;
+        private String deltaConfirmed;
+        private String deltaRecovered;
+        private String deltaDeceased;
 
         public OverviewStats() {
         }
 
-        public OverviewStats(String date, String confirmed, String recovered, String deceased) {
+        public OverviewStats(String date, String active, String confirmed, String recovered, String deceased, String deltaConfirmed, String deltaRecovered, String deltaDeceased) {
             this.date = date;
+            this.active = active;
             this.confirmed = confirmed;
             this.recovered = recovered;
             this.deceased = deceased;
+            this.deltaConfirmed = deltaConfirmed;
+            this.deltaRecovered = deltaRecovered;
+            this.deltaDeceased = deltaDeceased;
         }
 
         public String getDate() {
@@ -62,6 +70,14 @@ public class OverviewStatsAPIResponse {
 
         public void setDate(String date) {
             this.date = date;
+        }
+
+        public String getActive() {
+            return active;
+        }
+
+        public void setActive(String active) {
+            this.active = active;
         }
 
         public String getConfirmed() {
@@ -86,6 +102,30 @@ public class OverviewStatsAPIResponse {
 
         public void setDeceased(String deceased) {
             this.deceased = deceased;
+        }
+
+        public String getDeltaConfirmed() {
+            return deltaConfirmed;
+        }
+
+        public void setDeltaConfirmed(String deltaConfirmed) {
+            this.deltaConfirmed = deltaConfirmed;
+        }
+
+        public String getDeltaRecovered() {
+            return deltaRecovered;
+        }
+
+        public void setDeltaRecovered(String deltaRecovered) {
+            this.deltaRecovered = deltaRecovered;
+        }
+
+        public String getDeltaDeceased() {
+            return deltaDeceased;
+        }
+
+        public void setDeltaDeceased(String deltaDeceased) {
+            this.deltaDeceased = deltaDeceased;
         }
     }
 
@@ -145,6 +185,55 @@ public class OverviewStatsAPIResponse {
 
         public void setPublishedAt(String publishedAt) {
             this.publishedAt = publishedAt;
+        }
+    }
+
+    public static class OverviewSeries {
+        private String date;
+        private String confirmed;
+        private String recovered;
+        private String deceased;
+
+        public OverviewSeries() {
+        }
+
+        public OverviewSeries(String date, String confirmed, String recovered, String deceased) {
+            this.date = date;
+            this.confirmed = confirmed;
+            this.recovered = recovered;
+            this.deceased = deceased;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getConfirmed() {
+            return confirmed;
+        }
+
+        public void setConfirmed(String confirmed) {
+            this.confirmed = confirmed;
+        }
+
+        public String getRecovered() {
+            return recovered;
+        }
+
+        public void setRecovered(String recovered) {
+            this.recovered = recovered;
+        }
+
+        public String getDeceased() {
+            return deceased;
+        }
+
+        public void setDeceased(String deceased) {
+            this.deceased = deceased;
         }
     }
 }
